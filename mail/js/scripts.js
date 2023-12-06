7fc12674c1e98c8169435e7d08ff83244ed9d557
MirkoCifelli <ciffo4@hotmail.it>
2023-12-06 14:19:21 +0100

    Email not work

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -1,1 +1,40 @@
-console.log(`hello world`)
+// Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+
+// const verifiedEmail = ['email@gmail.com'];
+//     const emailScritta = document.getElementById('email');
+
+
+// for(let i= 0; i < verifiedEmail.length; i++){
+
+//     let email = verifiedEmail[i];
+//     if(email == emailScritta )
+//     email = true;
+// }
+
+// if (email== true){
+//     alert('evvai puoi entrare');
+// }
+// else{
+//     alert('voooleeeviii')
+// }
+const verifiedEmail = ['pippo'];
+const provaEmail = prompt('Inserisci la tua email')
+let email;
+
+for(let i= 0; i < verifiedEmail.length; i++){
+
+         let email = verifiedEmail[i];
+         if(email == provaEmail )
+         email = true;
+     }
+    
+     if (email == true){
+         alert('evvai puoi entrare');
+     }
+     else{
+         alert('voooleeeviii')
+     }
